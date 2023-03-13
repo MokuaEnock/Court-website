cac22b489682b1aa2ec639b2627d85c1a30be1f5
Enock <enock.mokua@student.moringaschool.com>
2023-03-13 07:44:16 +0300

    add content to signup

--- src/pages/landing/Laning.js
+++ src/pages/landing/Laning.js
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
 import "./Landing.css";
 
 export default function Landing() {
-  return <main>This is the Landing page</main>;
+  return (
+    <main className="landing">
+      <section className="landing-cont1">
+        This is the first container
+        <Link className="signup-button" to="/auth/signup">
+          Sign Up
+        </Link>
+      </section>
+      <section className="landing-cont1">This is the second container</section>
+    </main>
+  );
 }
