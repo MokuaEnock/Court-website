edcfacd05926fea2c2200ce5544958e588f2988b
Enock <enock.mokua@student.moringaschool.com>
2023-03-13 07:54:59 +0300

    create signup form

--- src/pages/authentication/Signup.js
+++ src/pages/authentication/Signup.js
@@ -1,5 +1,16 @@
 import "./auth.css";
 
 export default function Signup() {
-  return <section></section>;
+  return (
+    <main className="signup">
+      <form className="auth-form">
+        <p>Sign Up</p>
+        <input type="text" placeholder="Enter your username" />
+        <input type="text" placeholder="Enter your email address" />
+        <input type="text" placeholder="Enter your password" />
+        <input type="text" placeholder="Confirm your password" />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 }
