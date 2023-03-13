69d298b4709d76e7dcb665df303c1825e8c63519
Enock <enock.mokua@student.moringaschool.com>
2023-03-13 06:26:47 +0300

    create authentication components

---
+++ src/pages/authentication/Login.js
@@ -0,0 +1,5 @@
+import "./auth.css";
+
+export default function Login() {
+  return <section>This is the login page</section>;
+}
---
+++ src/pages/authentication/Signup.js
@@ -0,0 +1,5 @@
+import "./auth.css";
+
+export default function Signup() {
+  return <section>This is the signup page</section>;
+}
