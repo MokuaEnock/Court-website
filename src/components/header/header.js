d15f2321f1b6299f5f118857107b60ccfd9a55af
Enock <enock.mokua@student.moringaschool.com>
2023-03-13 08:16:48 +0300

    create sinup and login form

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
-  return <header>This is the header</header>;
+  return (
+    <header>
+      This is the header
+      <Link to="/auth/login">Login</Link>
+    </header>
+  );
 }
--- src/pages/authentication/Login.js
+++ src/pages/authentication/Login.js
@@ -1,5 +1,18 @@
 import "./auth.css";
 
 export default function Login() {
-  return <section>This is the login page</section>;
+  return (
+    <main className="login">
+      <form className="auth-form">
+        <p>Sign Up</p>
+        <input type="text" placeholder="Enter your username" />
+        <input type="text" placeholder="Enter your email address" />
+        <input type="text" placeholder="Enter your password" />
+        <input type="text" placeholder="Confirm your password" />
+        <button type="submit" className="submit-button">
+          Submit
+        </button>
+      </form>
+    </main>
+  );
 }
