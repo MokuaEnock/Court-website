a0cb3d70b1a80d89c6ab204be38d4a56183acb0e
Enock <enock.mokua@student.moringaschool.com>
2023-03-13 06:57:07 +0300

    add landing to its navigational path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import User from "./pages/user/User";
+import Landing from "./pages/landing/Laning";
 import Login from "./pages/authentication/Login";
 import Signup from "./pages/authentication/Signup";
 import Header from "./components/header/header";
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/user" element={<User />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/auth">
           <Route path="login" element={<Login />} />
           <Route path="signup" element={<Signup />} />
