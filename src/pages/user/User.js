da09d37820f3de1d7760b66becab01ad72711838
Enock <enock.mokua@student.moringaschool.com>
2023-03-13 09:00:41 +0300

    create scetion for text area

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,5 +1,30 @@
 import "./user.css";
 
 export default function User() {
-  return <main>This is the user page</main>;
+  return (
+    <main className="user">
+      <section className="user-container">
+        <h3>Upload your document</h3>
+        <form className="file-form">
+          <input
+            type="file"
+            placeholder="Upload file"
+            className="file-upload-input"
+          />
+        </form>
+      </section>
+
+      <section className="user-container">
+        <h3>Fill in your case</h3>
+        <form className="file-form">
+          <textarea
+            placeholder="Enter text"
+            className="file-upload-input"
+          />
+        </form>
+      </section>
+
+
+    </main>
+  );
 }
