d9606ee11a0fbb92b1b13fc456ac17fb00e41940
Enock <enock.mokua@student.moringaschool.com>
2023-03-13 06:39:27 +0300

    create default paths for react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Landing from "./pages/landing/Laning";
+import User from "./pages/user/User";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Landing />
+      <Routes>
+        <Route path="/user" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
