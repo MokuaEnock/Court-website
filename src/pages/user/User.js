321c69923e62bc595334e9a05ea17220aceb0ad9
Enock <enock.mokua@student.moringaschool.com>
2023-03-13 09:36:45 +0300

    specify files for upload

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -10,6 +10,7 @@ export default function User() {
             type="file"
             placeholder="Upload file"
             className="file-upload-input"
+            accept=".pdf, .docx"
           />
         </form>
       </section>
@@ -17,14 +18,14 @@ export default function User() {
       <section className="user-container">
         <h3>Fill in your case</h3>
         <form className="file-form">
-          <textarea
-            placeholder="Enter text"
-            className="file-upload-input"
-          />
+          <textarea placeholder="Enter text" className="file-upload-input" />
         </form>
       </section>
 
-
+      <section className="user-container">
+        <h3>Your output here</h3>
+        <p>Here is your output</p>
+      </section>
     </main>
   );
 }
